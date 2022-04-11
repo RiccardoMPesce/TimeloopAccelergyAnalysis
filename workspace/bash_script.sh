3480401e414e02c7cdf5f0e614048a43bedcf6c4
riccardompesce <riccardompesce@gmail.com>
2022-04-11 16:39:26 +0200

    Firs conf is eyeriss like

--- workspace/bash_script.sh
+++ workspace/bash_script.sh
@@ -2,45 +2,45 @@
 
 chmod -R 777 ./
 
-timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/prob/resnet18_layer1.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/constraints.yaml ResNet18/arch/components/*.yaml -o ./ResNet18/output/conf1/output1
+timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/arch/components/*.yaml ResNet18/prob/resnet18_layer1.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/*.yaml -o ./ResNet18/output/conf1/output1
 
-timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/prob/resnet18_layer2.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/constraints.yaml ResNet18/arch/components/*.yaml -o ./ResNet18/output/conf1/output2
+timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/arch/components/*.yaml ResNet18/prob/resnet18_layer2.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/*.yaml -o ./ResNet18/output/conf1/output2
 
-timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/prob/resnet18_layer3.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/constraints.yaml ResNet18/arch/components/*.yaml -o ./ResNet18/output/conf1/output3
+timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/arch/components/*.yaml ResNet18/prob/resnet18_layer3.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/*.yaml -o ./ResNet18/output/conf1/output3
 
-timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/prob/resnet18_layer4.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/constraints.yaml ResNet18/arch/components/*.yaml -o ./ResNet18/output/conf1/output4
+timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/arch/components/*.yaml ResNet18/prob/resnet18_layer4.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/*.yaml -o ./ResNet18/output/conf1/output4
 
-timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/prob/resnet18_layer5.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/constraints.yaml ResNet18/arch/components/*.yaml -o ./ResNet18/output/conf1/output5
+timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/arch/components/*.yaml ResNet18/prob/resnet18_layer5.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/*.yaml -o ./ResNet18/output/conf1/output5
 
-timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/prob/resnet18_layer6.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/constraints.yaml ResNet18/arch/components/*.yaml -o ./ResNet18/output/conf1/output6
+timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/arch/components/*.yaml ResNet18/prob/resnet18_layer6.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/*.yaml -o ./ResNet18/output/conf1/output6
 
-timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/prob/resnet18_layer7.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/constraints.yaml ResNet18/arch/components/*.yaml -o ./ResNet18/output/conf1/output7
+timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/arch/components/*.yaml ResNet18/prob/resnet18_layer7.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/*.yaml -o ./ResNet18/output/conf1/output7
 
-timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/prob/resnet18_layer8.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/constraints.yaml ResNet18/arch/components/*.yaml -o ./ResNet18/output/conf1/output8
+timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/arch/components/*.yaml ResNet18/prob/resnet18_layer8.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/*.yaml -o ./ResNet18/output/conf1/output8
 
-timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/prob/resnet18_layer9.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/constraints.yaml ResNet18/arch/components/*.yaml -o ./ResNet18/output/conf1/output9
+timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/arch/components/*.yaml ResNet18/prob/resnet18_layer9.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/*.yaml -o ./ResNet18/output/conf1/output9
 
-timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/prob/resnet18_layer10.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/constraints.yaml ResNet18/arch/components/*.yaml -o ./ResNet18/output/conf1/output10
+timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/arch/components/*.yaml ResNet18/prob/resnet18_layer10.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/*.yaml -o ./ResNet18/output/conf1/output10
 
-timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/prob/resnet18_layer11.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/constraints.yaml ResNet18/arch/components/*.yaml -o ./ResNet18/output/conf1/output11
+timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/arch/components/*.yaml ResNet18/prob/resnet18_layer11.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/*.yaml -o ./ResNet18/output/conf1/output11
 
-timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/prob/resnet18_layer12.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/constraints.yaml ResNet18/arch/components/*.yaml -o ./ResNet18/output/conf1/output12
+timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/arch/components/*.yaml ResNet18/prob/resnet18_layer12.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/*.yaml -o ./ResNet18/output/conf1/output12
 
-timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/prob/resnet18_layer13.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/constraints.yaml ResNet18/arch/components/*.yaml -o ./ResNet18/output/conf1/output13
+timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/arch/components/*.yaml ResNet18/prob/resnet18_layer13.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/*.yaml -o ./ResNet18/output/conf1/output13
 
-timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/prob/resnet18_layer14.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/constraints.yaml ResNet18/arch/components/*.yaml -o ./ResNet18/output/conf1/output14
+timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/arch/components/*.yaml ResNet18/prob/resnet18_layer14.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/*.yaml -o ./ResNet18/output/conf1/output14
 
-timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/prob/resnet18_layer15.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/constraints.yaml ResNet18/arch/components/*.yaml -o ./ResNet18/output/conf1/output15
+timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/arch/components/*.yaml ResNet18/prob/resnet18_layer15.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/*.yaml -o ./ResNet18/output/conf1/output15
 
-timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/prob/resnet18_layer16.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/constraints.yaml ResNet18/arch/components/*.yaml -o ./ResNet18/output/conf1/output16
+timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/arch/components/*.yaml ResNet18/prob/resnet18_layer16.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/*.yaml -o ./ResNet18/output/conf1/output16
 
-timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/prob/resnet18_layer17.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/constraints.yaml ResNet18/arch/components/*.yaml -o ./ResNet18/output/conf1/output17
+timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/arch/components/*.yaml ResNet18/prob/resnet18_layer17.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/*.yaml -o ./ResNet18/output/conf1/output17
 
-timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/prob/resnet18_layer18.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/constraints.yaml ResNet18/arch/components/*.yaml -o ./ResNet18/output/conf1/output18
+timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/arch/components/*.yaml ResNet18/prob/resnet18_layer18.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/*.yaml -o ./ResNet18/output/conf1/output18
 
-timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/prob/resnet18_layer19.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/constraints.yaml ResNet18/arch/components/*.yaml -o ./ResNet18/output/conf1/output19
+timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/arch/components/*.yaml ResNet18/prob/resnet18_layer19.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/*.yaml -o ./ResNet18/output/conf1/output19
 
-timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/prob/resnet18_layer20.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/constraints.yaml ResNet18/arch/components/*.yaml -o ./ResNet18/output/conf1/output20
+timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/arch/components/*.yaml ResNet18/prob/resnet18_layer20.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/*.yaml -o ./ResNet18/output/conf1/output20
 
-timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/prob/resnet18_layer21.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/constraints.yaml ResNet18/arch/components/*.yaml -o ./ResNet18/output/conf1/output21
+timeloop-mapper ResNet18/arch/ResNet1.yaml ResNet18/arch/components/*.yaml ResNet18/prob/resnet18_layer21.yaml ResNet18/mapper/mapper.yaml ResNet18/constraints/*.yaml -o ./ResNet18/output/conf1/output21
 
